3f5061b0f2eaf69610b9a0b2d96bbde4c6a3e850
伊藤 陽平 <ito@smcr.jp>
2021-02-07 21:02:23 +0900

    Update VerticalChart.js

--- src/components/VerticalChart.js
+++ src/components/VerticalChart.js
@@ -1,219 +1,123 @@
-import React, { useState } from "react";
-import {
-  ComposedChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  ReferenceLine,
-  Scatter,
-} from "recharts";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import BarChartPage from "./BarChart";
+import VerticalChartPage from "./VerticalChart";
+import Copyright from "./Copyright";
 import {
-  getPopulation,
-  isLoaded,
-  perPopulation,
-  getData,
-  citylistUrl,
-  patientUrl,
-  rtUrl,
-} from "../data/Utils";
-import Card from "@material-ui/core/Card";
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from "react-share";
+import Fade from "@material-ui/core/Fade";
+import Grow from "@material-ui/core/Grow";
+import ReactGA from "react-ga";
+import Link from "@material-ui/core/Link";
+
+ReactGA.initialize("UA-36217274-7");
+ReactGA.pageview("/");
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    maxWidth: "100%",
   },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
+  contentTitle: {
+    marginTop: "30px",
+    marginBottom: "30px",
+  },
+  divider: {
+    width: "30%",
+    marginBottom: "30px",
+  },
+  paragraph: {
+    marginBottom: "30px",
   },
   card: {
     padding: "5px",
   },
+  buttons: {
+    marginTop: "30px",
+    marginBottom: "10px",
+  },
+  fb_icon: {
+    marginRight: "10px",
+  },
+  attention: {
+    fontSize: "10px",
+  },
 }));
 
-function VerticalChartPage() {
-  const population = getPopulation();
+export default function Top() {
   const classes = useStyles();
-  const [citylist, setCitylist] = useState(false);
-  const [patient, setPatient] = useState(false);
-  const [rtList, setRtList] = useState(false);
-  const [graph, setGraph] = useState("graph1");
-
-  const handleGraphChange = (event) => {
-    setGraph(event.target.value);
-  };
-
-  if (!citylist) {
-    getData(citylistUrl, setCitylist);
-  }
-
-  if (!patient) {
-    getData(patientUrl, setPatient);
-  }
-
-  if (!rtList) {
-    getData(rtUrl, setRtList);
-  }
-
-  let cityLabel = [];
-  if (isLoaded(citylist)) {
-    citylist.forEach(function (city) {
-      cityLabel[city.code] = city.label;
-    });
-  }
-
-  let patientList = [];
-  let patientTemp;
-  if (isLoaded(patient) && (graph === "graph1" || graph === "graph2")) {
-    patientList = [];
-    for (let key in patient) {
-      patientTemp = patient[key];
-      patientList.push({
-        code: key,
-        cityName: cityLabel[key],
-        patient:
-          graph === "graph1"
-            ? Object.values(patientTemp).pop()
-            : perPopulation(Object.values(patientTemp).pop(), population[key]),
-      });
-    }
-    patientList.sort(function (a, b) {
-      if (a.patient < b.patient) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-  }
-
-  let rtData = [];
-  if (isLoaded(rtList) && graph === "graph3") {
-    patientList = [];
-    patientTemp = rtList.latest;
-    cityLabel.forEach(function (city) {
-      rtData = [0, [0, 0], [0, 0]];
-      for (let key in patientTemp) {
-        if (city === cityLabel[patientTemp[key].city]) {
-          rtData = [
-            patientTemp[key].ML,
-            [Number(patientTemp[key].Low_90), Number(patientTemp[key].High_90)],
-            [Number(patientTemp[key].Low_50), Number(patientTemp[key].High_50)],
-          ];
-        }
-        console.log(patientTemp);
-      }
-      patientList.push({
-        cityName: city,
-        patient: rtData,
-      });
-    });
-    patientList.sort(function (a, b) {
-      if (a.patient[0] < b.patient[0]) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-  }
   return (
-    <Card className={classes.card}>
-      <div>
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-simple-select-label">データの種類</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={graph}
-            onChange={handleGraphChange}
+    <Fade in={true} timeout={{ enter: 1300 }}>
+      <main className={classes.content}>
+        <Toolbar />
+        <Typography
+          className={classes.contentTitle}
+          variant="h4"
+          component="h2"
+        >
+          東京都23区市町村別感染者グラフ
+        </Typography>
+        <Divider className={classes.divider} />
+        <Typography paragraph className={classes.paragraph}>
+          東京都が公開している新型コロナ感染症に関するデータを基礎自治体目線で整形し、累計、日別、10万人あたり、実効再生数(Rt)で表現しています。
+          <br />
+          <span paragraph className={classes.attention}>
+            ※都と区市町村が公開する感染者数が異なる場合があります。
+            <br />
+            ※4/12練馬区が60から59へと減少する挙動は元データを反映したものです。
+            <br />
+            ※Rtのプログラムは、
+            <Link
+              target="_blank"
+              color="inherit"
+              rel="noopner noreferer"
+              href="https://rt-live-japan.com/"
+            >
+              Rt Covid-19 Japanさま
+            </Link>
+            のコードをフォークし開発しました。
+            <br />
+            ※感染者数が少ない場合、Rtで大きな数字が出る場合があります。
+          </span>
+        </Typography>
+        <div className={classes.buttons}>
+          <FacebookShareButton
+            url={"https://stopcovid19.codeforshinjuku.org/"}
+            className={classes.fb_icon}
           >
-            <MenuItem value={"graph1"}>区市町村感染者累計</MenuItem>
-            <MenuItem value={"graph2"}>区市町村10万人あたり</MenuItem>
-            <MenuItem value={"graph3"}>区市町村実効再生産数(Rt)</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-      <div style={{ marginTop: 30, width: "100%", height: "2000px" }}>
-        <ResponsiveContainer>
-          <ComposedChart
-            layout="vertical"
-            data={patientList}
-            margin={{
-              top: 20,
-              right: 0,
-              bottom: 20,
-              left: 30,
-            }}
+            <FacebookIcon size={30} round />
+          </FacebookShareButton>
+          <TwitterShareButton
+            url={"https://stopcovid19.codeforshinjuku.org/"}
+            title={"都内区市町村に特化した感染者グラフ | 東京新型コロナグラフ"}
+            hashtags={["StopCOVID19JP"]}
           >
-            <CartesianGrid stroke="#f5f5f5" />
-            {graph === "graph3" ? (
-              <XAxis ticks={[0.5, 1.0, 2.0, 3.0, 4.0, 5.0]} type="number" />
-            ) : (
-              <XAxis type="number" />
-            )}
-            <YAxis dataKey="cityName" type="category" />
-            <Tooltip />
-            <Legend verticalAlign="top" />
-            {(graph === "graph1" || graph === "graph2") && (
-              <Bar
-                dataKey="patient"
-                barSize={15}
-                fill="#8884d8"
-                name="感染者数"
-              />
-            )}
-            {graph === "graph3" && (
-              <Bar
-                dataKey="patient[1]"
-                barSize={15}
-                fill="#82ca9d"
-                name="信頼区間90%"
-                isAnimationActive={false}
-              />
-            )}
-            {graph === "graph3" && (
-              <Bar
-                dataKey="patient[2]"
-                barSize={15}
-                fill="#ffc658"
-                name="信頼区間50%"
-                opacity="0.2"
-                isAnimationActive={false}
-              />
-            )}
-            {graph === "graph3" && (
-              <Scatter
-                dataKey="patient[0]"
-                fill="#8884d8"
-                name="最頻値"
-                isAbove
-              />
-            )}
-            {graph === "graph3" && (
-              <ReferenceLine
-                x={1}
-                stroke="red"
-                label={{
-                  position: "top",
-                  value: "1.0",
-                  fontSize: 14,
-                }}
-              />
-            )}
-          </ComposedChart>
-        </ResponsiveContainer>
-      </div>
-    </Card>
+            <TwitterIcon size={30} round />
+          </TwitterShareButton>
+        </div>
+        <Typography paragraph>
+          <Grow in={true} timeout={{ enter: 2000 }}>
+            <BarChartPage />
+          </Grow>
+          <VerticalChartPage />
+        </Typography>
+        <span paragraph className={classes.attention}>
+          23区(特別区)
+          千代田区、港区、中央区、新宿区、文京区、墨田区、江東区、品川区、目黒区、大田区、世田谷区、渋谷区、中野区、杉並区、豊島区、北区、荒川区、板橋区、練馬区、足立区、葛飾区、江戸川区
+          <br />
+          市町村
+          八王子市、立川市、武蔵野市、三鷹市、青梅市、府中市、昭島市、調布市、町田市、小金井市、小平市、日野市、東村山市、国分寺市、国立市、福生市、狛江市、東大和市、清瀬市、東久留米市、武蔵村山市、多摩市、稲城市、羽村市、あきる野市、西東京市、瑞穂町、日の出町、檜原村、奥多摩町、大島町、利島村、新島村、神津島村、三宅村、御蔵島村、八丈町、青ヶ島村、小笠原村
+        </span>
+        <Copyright />
+      </main>
+    </Fade>
   );
 }
-
-export default VerticalChartPage;
